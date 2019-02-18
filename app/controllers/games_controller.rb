d66301dc6ad4bda476da1fb169a4762f7b66f4bd
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-02-18 15:06:56 +1100

    Longest Word Game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.shuffle[0...10]
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters].split(' ')
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    data = JSON.parse(open(url).read)
+
+    @word.split('').sort.each do |letter|
+      if @letters.include?(letter)
+        @answer = "Well Done!"
+      else
+        @answer = "Wrong Letters Used!"
+      end
+    end
+
+    if data["found"] == false
+      @answer = "Word Doesn't Exist!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  post 'score', to: 'games#score', as: :score
+  root to: 'games#new', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
